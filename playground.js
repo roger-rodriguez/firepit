9a73a00d7534619cc8420f2eab552409e66972b8
Salakar <mike.diarmid@gmail.com>
2017-10-13 00:53:16 +0100

    updated playground

--- playground.js
+++ playground.js
@@ -7,9 +7,21 @@ const app = firebase.initializeApp({
   databaseURL: "https://firepit-tests.firebaseio.com"
 });
 
-firepit.use(app);
+firepit.use(app, {
+  // default models config goes here
+  scoobyDoo() {
+    console.log('where are you');
+  },
+  attributes: {
+    scoobyDoo: {
+      type: 'string',
+      defaultsTo: 'where are you',
+    }
+  },
+});
 
-firebase.firepit().createModel('User', {
+// this now returns
+const alsoReturnsTheModel = firebase.firepit().createModel('User', {
   attributes: {
     firstName: 'string',
     lastName: 'string',
@@ -22,7 +34,7 @@ firebase.firepit().createModel('User', {
   },
 });
 
-
+// todo move to internals and automatically init on first module use
 firebase.firepit().initialize();
 
 const User = firebase.firepit().model('User');
@@ -37,109 +49,3 @@ User
   .catch((error) => {
     console.log(error.message); // Users must be over 18 years old!
   });
-
-
-// firebase.firepit().model('User', {
-//   attributes: {
-//     name: { type: 'string' },
-//     email: { type: 'email' },
-//     age: { type: 'integer' },
-//     noob: { type: 'boolean', defaultsTo: true },
-//     noobs: {
-//       type: 'string',
-//       enum: ['1', '2'],
-//       defaultsTo: '2'
-//     },
-//     doop: {
-//       type: 'string',
-//       minLength: 3,
-//       maxLength: 4,
-//     }
-//     // posts: {
-//     //   collection: 'posts',
-//     // }
-//   }
-// });
-//
-// firebase.database();
-//
-// firebase.firepit().initialize({
-//   scoobyDoo() {
-//     console.log('where are you');
-//   },
-//   attributes: {
-//     scoobyDoo: {
-//       type: 'string',
-//       defaultsTo: 'where are you',
-//     }
-//   },
-// });
-//
-// // User.findOne().then((val) => {
-// //   debugger;
-// // }).catch((error) => {
-// //   debugger;
-// // });
-//
-//
-// //
-// // const userDefaults = {
-// //   //A flag to toggle schemaless or schema mode in databases that support schemaless data structures. If turned off,
-// //   // this will allow you to store arbitrary data in a record. If turned on, only attributes defined in the model's attributes object will be stored.
-// //   schema: true, // sc
-// //
-// //   identity: 'users', // The lowercase unique key for this model, e.g. user.
-// //
-// //   app: '[DEFAULT]', // app instance
-// //
-// //   autoId: true,
-// //
-// //   autoCreatedAt: true,
-// //
-// //   autoUpdatedAt: true,
-// //
-// //   autoUpdatedBy: true, // current authed user
-// //
-// //   autoCreatedBy: true, // current authed user
-// //
-// //   collectionName: 'user',
-// // };
-// //
-// //
-// // const User = firepit.model('User', {
-// //   ...userDefaults,
-// //   attributes: {
-// //     name: { type: 'string' },
-// //     email: { type: 'email' },
-// //     age: { type: 'integer' },
-// //     noob: { type: 'boolean' },
-// //     posts: {
-// //       collection: 'posts',
-// //     }
-// //   }
-// // });
-// //
-// // User.findByAge(55).then((val) => {
-// //   debugger;
-// // }).catch((error) => {
-// //   debugger;
-// // });
-// //
-// // User.findByName('elliot').then((val) => {
-// //   debugger;
-// // }).catch((error) => {
-// //   debugger;
-// // });
-// //
-// // User.findOne().then((val) => {
-// //   debugger;
-// // }).catch((error) => {
-// //   debugger;
-// // });
-// //
-// // User.find({ name: 'elliot', age: 12, noob: true, }).then((val) => {
-// //   debugger;
-// // }).catch((error) => {
-// //   debugger;
-// // });
-// //
