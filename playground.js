30d183479a8580bcfddbbc056442ffb220c4b417
Salakar <mike.diarmid@gmail.com>
2017-10-14 19:55:00 +0100

    various playground examples

--- playground.js
+++ playground.js
@@ -26,38 +26,12 @@ const alsoReturnsTheModel = firebase.firepit().createModel('User', {
     name: {
       type: 'string',
     },
-    transactionId: {
-      type: 'string',
-      defaultsTo() {
-        return Math.random().toString();
-      }
-    },
-    fieldMadeFromOtherFields: {
-      type: 'string',
-      defaultsTo(otherFields) {
-        return otherFields.a + otherFields.c + otherFields.z;
-      }
-    },
     noob: {
       type: 'boolean',
     },
     age: {
       type: 'integer',
     },
-    posts: {
-      collection: 'post',
-    },
-    company: {
-      model: 'company',
-    }
-
-
-    // age: {
-    //   type: 'integer',
-    //   validate(age) {
-    //     if (age < 18) throw new Error('Users must be over 18 years old!');
-    //   },
-    // },
   },
 });
 
@@ -65,108 +39,20 @@ firebase.firepit().initialize();
 
 const User = firebase.firepit().model('User');
 
-Company
-  .findOneByName('Provide the Solutions Limited')
-  // .populate('posts', {
-  //   comments_count: {
-  //     $gte: 10,
-  //   }
-  // })
-  .then((company) => {
-    const ref = company.ref;
-
-    User.create({
-      name: 'ewehrehjt',
-      company: ref,
-    }).then
-
-    // for (let i = 0, len = usersArray.length; i < len; i++) {
-    //   const user = usersArray[i];
-    //   console.log(user);
-    // }
-    //
-    // usersArray.nextPage().then(() => {
-    //
-    // })
+User
+  // .find({ name: 'elliot' })
+  .findOneByName('elliot')
+  .where({ age: 12 })
+  // .findOne('dJbQltnAsv4wKuJTdxZU')
+  // .sort('age', 'asc')
+  // .sort({ age: 1 })
+  // .sort({ age: -1 })
+  // .sort('age', 'desc')
+  // .limit(1)
+  .then((result) => {
+    console.dir(result);
   })
   .catch((error) => {
-    console.log(error.message); // Users must be over 18 years old!
+    console.error(error);
   });
 
-//
-//
-// User
-//   .find({ noob: true })
-//   .page(1)
-//   .limit(10)
-//   .then()
-
-// Users-- -
-// UserPosts - ^
-// UserPostComments - ^
-
-// before
-// firestore
-//   .collection('users')
-//   .doc('id')
-//   .collection('posts')
-//   .doc('id')
-//   .collection('comments')
-//   .where('title', '==', 'chickens')
-//   .where('creator', '==', 'bob')
-//   .get()
-//   .then((post) => {
-//
-//   });
-
-// after
-// UserPostComments({ user_id: '1', post_id: '90', })
-//   .findOne({
-//     title: 'chickens',
-//     creator: 'bob',
-//     likes: {
-//       $gte: 10,
-//       $lte: 100,
-//     }
-//   })
-//   .then(() => {
-//
-//   });
-
-//
-
-//
-// firestore.collection('users').doc('id').collection('posts')
-// firestore.doc('users/id/posts/id/comments/id').doc('id').collection('posts')
-// firestore.collection('users').doc('id').collection('posts').doc('id').collection('comments').where('test', '==', 'fack u').get()
-//
-// User.findOne({ name: 'elliot' }).where({ age: 12 }).populate('posts');
-/*
------------------------------
-User A > Assigned to Device A
-        \/ ---- /\
-Device A > Assigned to User A
------------------------------
-
-Sometime Later:
-User A gets Newly assigned to
-Device B:
-
------------------------------
-User A > Assigned to Device B
-        \/ ---- /\
-Device B > Assigned to User A
------------------------------
-
-But... Device A still believes
-it belongs to User A, even though
-the link has been broken:
-
------------------------------
-Device A > Assigned to User A
-          X ---- X
-User A > Assigned to Device B
------------------------------
-
-
-*/
