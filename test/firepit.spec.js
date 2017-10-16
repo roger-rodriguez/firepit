17d5f0278337c9495d3d19975241b3ed5f0261db
Elliot Hesp <elliot.hesp@gmail.com>
2017-10-16 20:40:13 +0100

    Update firepit core tests

--- test/firepit.spec.js
+++ test/firepit.spec.js
@@ -2,4 +2,8 @@ const firepit = require('../src');
 
 describe('firepit', () => {
 
+  it('should export with use method', () => {
+    firepit.should.have.property('use').and.be.instanceOf(Function);
+  });
+
 });
