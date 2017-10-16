365b6b3605e30434d1fbcc9a989591738a5f31f9
Elliot Hesp <elliot.hesp@gmail.com>
2017-10-16 10:06:28 +0100

    Add findOne/limit/where spec tests

---
+++ test/query.spec.js/methods/findOne.spec.js
@@ -0,0 +1,27 @@
+const Model = require('../../../src/Model');
+const Query = require('../../../src/Query');
+const internals = require('../../../src/internals');
+
+const testAppName = 'TestApp';
+let testApp;
+let modelInstance;
+
+describe('Query findOne method', () => {
+
+  beforeEach(() => {
+    testApp = internals.createInstance({
+      name: testAppName,
+    });
+    modelInstance = new Model(testAppName, 'Test', { schema: false });
+  });
+
+  it('should set internal flag to true when called', () => {
+    const query = new Query(modelInstance, {}).isFindOne('foobar');
+    query.should.have.property('_isFindOne', true);
+  });
+
+  afterEach(() => {
+    internals.deleteInstance(testAppName);
+  });
+
+});
