5ef73cdd05f89776e30196c1ca74036705b3d64f
Salakar <mike.diarmid@gmail.com>
2017-10-16 16:05:36 +0100

    misc cleanup

--- src/internals/ModelInternal.js
+++ src/internals/ModelInternal.js
@@ -1,6 +1,7 @@
 const Schema = require('../Schema');
 const { UTILS } = require('./');
 const { toFirstUpper } = UTILS;
+
 /**
  *
  * @param model
@@ -11,14 +12,20 @@ function attachMagicMethods(model) {
   for (let i = 0, len = fields.length; i < len; i++) {
     const field = fields[i];
     const fieldName = toFirstUpper(field);
-    const fieldProperties = model.schema.attributes[field]; // todo
-    // TODO should transform fields like first_name to work like: findByFirstName - no underscores
+    // const fieldProperties = model.schema.attributes[field]; // todo
+    // // TODO should transform fields like first_name to work like: findByFirstName - no underscores
     model[`findBy${fieldName}`] = model.findByField.bind(model, field);
     model[`findOneBy${fieldName}`] = model.findOneByField.bind(model, field);
   }
 }
 
 class BaseModel {
+  /**
+   *
+   * @param appName
+   * @param modelName
+   * @param schema
+   */
   constructor(appName, modelName, schema) {
     this.appName = appName;
     this.modelName = modelName;
