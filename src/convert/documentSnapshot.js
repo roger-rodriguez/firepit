ff7286d0eeaaa63840fd675fc701ab5c088b6806
Salakar <mike.diarmid@gmail.com>
2017-10-17 18:31:45 +0100

    DocumentSnapshot converter

---
+++ src/convert/documentSnapshot.js
@@ -0,0 +1,46 @@
+module.exports = function documentSnapshot(model, documentSnapshot) {
+  if (!documentSnapshot || !documentSnapshot.exists) return null;
+
+  const out = { id: documentSnapshot.id };
+  const schema = model.schema.schema;
+  const data = documentSnapshot.data();
+  const attributes = Object.keys(schema ? model.schema.attributes : Object.assign({}, data, model.schema.attributes));
+
+  for (let i = 0, len = attributes.length; i < len; i++) {
+    out[attributes[i]] = data[attributes[i]];
+  }
+
+  Object.defineProperties(out, {
+    ref: {
+      enumerable: false,
+      value: model.nativeCollection.doc(out.id),
+    },
+    model: {
+      enumerable: false,
+      value: model,
+    },
+    // should
+    save: {
+      enumerable: false,
+      value: (function save() {
+        const id = out.id;
+        // todo only update changes and not whole doc?
+        // todo use firestore.FieldValue.delete() for deleted keys
+        // todo update updateAt timestampValue
+        delete out.id;
+        return model.updateOne(id, out);
+      }).bind(out),
+    },
+    destroy: {
+      enumerable: false,
+      value: (function destroy() {
+        return model.destroy(out.id);
+      }).bind(out),
+    },
+  });
+
+  // todo attach virtual attributes
+
+  return out;
+};
+
