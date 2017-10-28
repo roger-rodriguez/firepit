e677eca88ff7063c5458c8cbe461c252c8fec08f
Elliot Hesp <elliot.hesp@gmail.com>
2017-10-28 12:03:34 +0100

    Add destroy spec

---
+++ test/integration/model/destroy.spec.js
@@ -0,0 +1,98 @@
+let firebase = null;
+
+const model = 'Test_Schema';
+let idToUpdate = null;
+const original = { name: 'foobar', age: 123, deep: { foo: 'bar' } };
+
+describe('destroy', () => {
+
+  before(() => {
+    firebase = require('../app')().firebase;
+
+    firebase.firepit().createModel(model, {
+      attributes: {
+        name: {
+          type: 'string',
+          required: true,
+        },
+        age: {
+          type: 'integer',
+          required: true,
+        }
+      },
+    });
+  });
+
+  it('should throw if object and ID is passed', () => {
+    const User = firebase.firepit().model(model);
+
+    return User
+      .destroy({
+        id: '123',
+      })
+      .then(() => {
+        throw new Error('Document should not have been destroyed');
+      })
+      .catch(() => {
+        // todo error
+        return Promise.resolve();
+      });
+  });
+
+  it('should throw if array containing non-string is passed', () => {
+    const User = firebase.firepit().model(model);
+
+    return User
+      .destroy(['123', 456])
+      .then(() => {
+        throw new Error('Document should not have been destroyed');
+      })
+      .catch(() => {
+        // todo error
+        return Promise.resolve();
+      });
+  });
+
+  it('should destroy array of string documents', () => {
+    const User = firebase.firepit().model(model);
+
+    let id = null;
+
+    return User
+      .create({
+        name: 'foobar',
+        age: 123
+      })
+      .then((created) => {
+        id = created.id;
+        return User.destroy([id]);
+      })
+      .then(() => User.findOneById(id))
+      .then((document) => {
+        if (document !== null) throw new Error('Document returned after it should have been destroyed');
+      });
+  });
+
+  it('should destroy documents by query', () => {
+    const User = firebase.firepit().model(model);
+
+    let id = null;
+
+    const toDestroy = {
+      name: `${Math.random().toString(32)}`,
+      age: 123,
+    };
+
+    return User
+      .create(toDestroy)
+      .then((created) => {
+        id = created.id;
+        return User.destroy(toDestroy);
+      })
+      .then(() => User.findOneById(id))
+      .then((document) => {
+        if (document !== null) throw new Error('Document returned after it should have been destroyed');
+      });
+  });
+
+});
